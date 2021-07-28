e56cac998ac049cf1318819f5921da41786f3eee
renzothemartian <smelter03@gmail.com>
2021-07-28 10:53:40 -0600

    oof

--- pup.sh
+++ pup.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
+gg="/"
 read -p "Enter your download repository: "  repo
 read -p "Enter your package to install: "  inst
-URL="$repo $inst"
+URL="$repo$gg$inst"
 print $URL
 $FILE="inst.sh"
 LOGFILE=wget.log
 wget $URL $FILE
-bash inst.sh
