8be2428b517fa08267456f21b65730457a95412f
renzothemartian <smelter03@gmail.com>
2021-07-28 10:37:00 -0600

    2 test

--- pup.sh
+++ pup.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 read -p "Enter your download repository: "  repo
-read -p "Enter your package to install: "  /inst
-URL="$repo $/inst"
+read -p "Enter your package to install: "  inst
+URL="$repo $inst"
 $FILE="inst.sh"
 wget $URL -O $FILE
 bash inst.sh
